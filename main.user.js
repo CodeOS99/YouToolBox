b882b12d566a61c47c5f6467e2288f655c691af5
Kushagra Sharma <kush.kuhu@gmail.com>
2025-06-20 19:18:59 +0530

    Added a menu with a note taking tab functioning, without local storage. Also added the buttons needed for a calculator function.

--- main.user.js
+++ main.user.js
@@ -10,11 +10,15 @@
 
 (function () {
     'use strict';
+    console.log("YTB Active!");
+
+    // COLOURS
+    const lightGray = '#A9A9A9';
+    const darkRed = '#8B0000';
+    const darkerRed = '#900C3F';
 
     let menuHeld = false; // if the menu is held
     let menuOx = 0, menuOy = 0; // menu offset x and y
-
-    let showMenu = false;
 
     // Wait until the page is fully ready to add stuff
     window.addEventListener('load', () => {
@@ -70,6 +74,198 @@
             cursor: 'move',
         });
 
+        let topMenuDiv = document.createElement('div');
+        Object.assign(topMenuDiv.style, {
+            width: '200px',
+            height: '50px',
+            backgroundColor: '#444',  // make it visible
+            marginLeft: '10px',
+            borderRadius: '8px',
+            display: 'none',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: '#fff',
+            fontWeight: 'bold',
+            fontFamily: 'sans-serif',
+        });
+
+        let closeMenuBtn = document.createElement('button');
+        closeMenuBtn.innerText = "X"
+        Object.assign(closeMenuBtn.style, {
+            top: '20px',
+            left: '20px',
+            width: '20px',
+            zIndex: 9999,
+            marginLeft: '10px',
+            fontSize: '14px',
+            background: 'red',
+            color: '#fff',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'move',
+            fontSize: '10px',
+        });
+
+        let noteModeButton = document.createElement('button');
+        noteModeButton.innerText = "NOTES"
+        Object.assign(noteModeButton.style, {
+            top: '80px',
+            left: '20px',
+            width: '50px',
+            zIndex: 9999,
+            marginLeft: '10px',
+            fontSize: '14px',
+            background: lightGray,
+            color: '#fff',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'move',
+            fontSize: '10px'
+        });
+
+        let calculatorBtn = document.createElement('button');
+        calculatorBtn.innerText = "CALC."
+        Object.assign(calculatorBtn.style, {
+            top: '80px',
+            left: '20px',
+            width: '50px',
+            zIndex: 9999,
+            marginLeft: '10px',
+            fontSize: '14px',
+            background: lightGray,
+            color: '#fff',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'move',
+            fontSize: '10px'
+        });
+
+        let codeSandboxBtn = document.createElement('button');
+        codeSandboxBtn.innerText = "CODE"
+        Object.assign(codeSandboxBtn.style, {
+            top: '80px',
+            left: '20px',
+            width: '50px',
+            zIndex: 9999,
+            marginLeft: '10px',
+            marginRight: '10px',
+            fontSize: '14px',
+            background: lightGray,
+            color: '#fff',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'move',
+            fontSize: '10px'
+        });
+
+        topMenuDiv.appendChild(closeMenuBtn);
+        topMenuDiv.appendChild(noteModeButton);
+        topMenuDiv.appendChild(calculatorBtn);
+        topMenuDiv.appendChild(codeSandboxBtn);
+
+        let menuBtns = [noteModeButton, calculatorBtn, codeSandboxBtn];
+        menuBtns.forEach(btn => {
+            btn.addEventListener('mouseover', () => {
+                btn.style.backgroundColor = 'gray';
+            });            
+            btn.addEventListener('mouseleave', () => {
+                btn.style.backgroundColor = lightGray;
+            });
+        });
+
+        closeMenuBtn.onmouseover = () => {
+            closeMenuBtn.style.backgroundColor = darkRed;
+        };            
+        closeMenuBtn.onmouseleave = () => {
+            closeMenuBtn.style.backgroundColor = 'red';
+        };
+        closeMenuBtn.onmousedown = () => {
+            closeMenuBtn.style.backgroundColor = darkerRed;
+        }
+
+        let bottomMenuDiv = document.createElement('div');        
+        Object.assign(bottomMenuDiv.style, {
+            width: '200px',
+            height: '50px',
+            backgroundColor: '#444',  // make it visible
+            marginLeft: '10px',
+            borderRadius: '8px',
+            display: 'none',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: '#fff',
+            fontWeight: 'bold',
+            fontFamily: 'sans-serif',
+            resize: 'both',
+            overflow: 'auto'
+        });
+
+        let noteInput = document.createElement('textarea');
+        
+        const buttonLabels = [
+            '7', '8', '9', '/',
+            '4', '5', '6', '*',
+            '1', '2', '3', '-',
+            '0', '.', '=', '+',
+            'CLR', 'DEL'
+        ];
+        let calculatorBtns = [];
+
+        let calculatorBtnDiv = document.createElement('div');
+        Object.assign(calculatorBtnDiv.style, {
+            display: 'none',
+            gridTemplateColumns: 'repeat(4, 50px)',
+            gap: '6px',
+            padding: '10px',
+            justifyContent: 'center',
+            alignItems: 'center'
+        });
+
+        bottomMenuDiv.appendChild(calculatorBtnDiv);
+
+        for (let i = 0; i < buttonLabels.length; i++) {
+            let btn  = document.createElement('button');
+            btn.innerText = buttonLabels[i];
+
+            Object.assign(btn.style, {
+                backgroundColor: lightGray,
+                color: 'white',
+                border: 'none',
+                borderRadius: '5px',
+                padding: '15px',
+                fontSize: '16px',
+                cursor: 'pointer',
+                width: '50px',
+                height: '50px',
+            });
+
+            btn.onmouseenter = () => {
+                btn.style.backgroundColor = 'gray';
+            }
+
+            btn.onmouseleave = () => {
+                btn.style.backgroundColor = lightGray
+            }
+
+            calculatorBtnDiv.appendChild(btn);
+
+            calculatorBtns.push(btn);
+        }
+        let showCalculator = () => {
+            calculatorBtnDiv.style.display = 'grid';
+            for(let btn of calculatorBtns) {
+                btn.style.display = 'block';
+            }
+
+            noteInput.style.display = 'none';
+        }
+
+        bottomMenuDiv.appendChild(noteInput);
+
+        calculatorBtn.onmousedown = () => {
+            showCalculator();
+        }
+
         mover.onmousedown = (e) => {
             if(!menuHeld) {
                 menuHeld = true;
@@ -91,11 +287,20 @@
         }
 
         main_menu_btn.onclick = () => {
-            showMenu = true;
+            main_menu_btn.style.display = 'none';
+            //mover.style.display = 'none';
+
+            topMenuDiv.style.display = 'flex';
+            bottomMenuDiv.style.display = 'flex';
+            div.style.flexDirection = 'column';
         };
 
         div.appendChild(mover);
         div.appendChild(main_menu_btn);
+
+        div.appendChild(topMenuDiv);
+        div.appendChild(bottomMenuDiv);
+
         document.body.appendChild(div);
     }
 })();
