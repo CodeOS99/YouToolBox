9b738f38170480d26ca1da2fc7f406130210acbf
Kushagra Sharma <kush.kuhu@gmail.com>
2025-06-20 17:09:03 +0530

    Added a moveable button

--- main.user.js
+++ main.user.js
@@ -14,21 +14,50 @@
     let menuHeld = false; // if the menu is held
     let menuOx = 0, menuOy = 0; // menu offset x and y
 
+    let showMenu = false;
+
     // Wait until the page is fully ready to add stuff
     window.addEventListener('load', () => {
-        makeMenuButton(); // make the menu button initially
+        makeMenu(); // make the menu button initially
     });
 
-    function makeMenuButton() {
+    function makeMenu() {
         let div = document.createElement('div');
-        div.id = "main-menu"
-        let btn = document.createElement('button');
-        btn.id = 'main-menu-btn'
-
-        btn.textContent = 'Show menu?';
-
-        Object.assign(btn.style, {
+        div.id = "main-menu";
+        Object.assign(div.style, {
             position: 'fixed',
+            top: '80px',
+            left: '20px',
+            zIndex: 9999,
+            display: 'flex',
+            alignItems: 'center',
+            gap: '1px',
+            cursor: 'move', // Make the whole group feel draggable
+            userSelect: 'none',
+        });
+
+        let mover = document.createElement('div');
+        Object.assign(mover.style, {
+            width: '50px',
+            height: '50px',
+            backgroundColor: '#444',  // make it visible
+            marginLeft: '10px',
+            borderRadius: '8px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: '#fff',
+            fontWeight: 'bold',
+            fontFamily: 'sans-serif',
+        });
+        mover.textContent = 'Move';
+
+        let main_menu_btn = document.createElement('button');
+        main_menu_btn.id = 'main-menu-btn'
+
+        main_menu_btn.textContent = 'Show menu?';
+
+        Object.assign(main_menu_btn.style, {
             top: '80px',
             left: '20px',
             zIndex: 9999,
@@ -41,30 +70,32 @@
             cursor: 'move',
         });
 
-        btn.onmousedown = (e) => {
-            menuHeld = true;
-            menuOx = e.clientX - btn.offsetLeft;
-            menuOy = e.clientY - btn.offsetTop;
-            document.body.style.userSelect = 'none';
-        }
-
-        btn.onmouseup = () => {
-            menuHeld = false;
-        }
-
-        btn.onmousemove = (e) => {
-            if(menuHeld) {
-                btn.style.left = (e.clientX - menuOx) + 'px';
-                btn.style.top = (e.clientY - menuOy) + 'px';
+        mover.onmousedown = (e) => {
+            if(!menuHeld) {
+                menuHeld = true;
+                menuOx = e.clientX - div.offsetLeft;
+                menuOy = e.clientY - div.offsetTop;
+                document.body.style.userSelect = 'none';
             }
         }
 
-        btn.onclick = () => {
-            alert('You clicked the button!');
+        document.onmouseup = () => { // add the event listener to the body because there is a possibility for the mouse to move away from the button
+            menuHeld = false;
+        }
+
+        document.onmousemove = (e) => {
+            if(menuHeld) {
+                div.style.left = (e.clientX - menuOx) + 'px';
+                div.style.top = (e.clientY - menuOy) + 'px';
+            }
+        }
+
+        main_menu_btn.onclick = () => {
+            showMenu = true;
         };
 
-        div.appendChild(btn);
+        div.appendChild(mover);
+        div.appendChild(main_menu_btn);
         document.body.appendChild(div);
-
     }
 })();
